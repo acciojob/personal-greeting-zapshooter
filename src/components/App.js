518a08c9c748e5a49827a1379f314b03ba122632
zapshooter <92568875+zapshooter@users.noreply.github.com>
2025-10-03 18:56:13 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,31 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [name, setName] = useState('');
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <label htmlFor="username" style={{ fontSize: '1.5rem' }}>
+        Enter your name:
+      </label>
+      <br />
+      <input
+        type="text"
+        id="username"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        style={{ width: '300px', height: '32px', fontSize: '1.1rem', marginTop: '16px' }}
+        placeholder=""
+      />
+      <div style={{ marginTop: '24px', fontSize: '1.4rem', fontWeight: '500' }}>
+        {name && (
+          <>Hello, {name}!</>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
