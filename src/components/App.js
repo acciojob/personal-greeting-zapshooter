ab06f2f3d9ae229141409998f660b6ad5528046c
zapshooter <92568875+zapshooter@users.noreply.github.com>
2025-10-03 19:08:59 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -7,23 +7,13 @@ const App = () => {
   return (
     <div>
       {/* Do not remove the main div */}
-      <label htmlFor="username" style={{ fontSize: '1.5rem' }}>
-        Enter your name:
-      </label>
-      <br />
       <input
         type="text"
-        id="username"
         value={name}
-        onChange={(e) => setName(e.target.value)}
-        style={{ width: '300px', height: '32px', fontSize: '1.1rem', marginTop: '16px' }}
-        placeholder=""
+        onChange={e => setName(e.target.value)}
       />
-      <div style={{ marginTop: '24px', fontSize: '1.4rem', fontWeight: '500' }}>
-        {name && (
-          <>Hello, {name}!</>
-        )}
-      </div>
+      {/* Only display greeting if input is NOT strictly empty */}
+      {name !== '' && <p>Hello, {name}!</p>}
     </div>
   );
 };
